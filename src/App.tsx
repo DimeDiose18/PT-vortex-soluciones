1fea21e4025473d3b5dbda200f4a89d8f447a61d
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2024-01-05 21:12:09 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hola from './components/Hola'
+
+function App() {
+
+  return (
+    <>
+      <Hola />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hola.tsx
@@ -0,0 +1,10 @@
+
+const Hola = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hola
